c0049c58ce9b0c1bdbab48306837a747eea29f57
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-16 13:51:09 +0200

    Creating list
    (cherry picked from commit c22f60ba27784118895a0cdac8290a8fdc1760bc)

--- src/main/kotlin/ru/otus/homework/Collections.kt
+++ src/main/kotlin/ru/otus/homework/Collections.kt
@@ -1,37 +1,26 @@
 package ru.otus.homework
 
 fun main() {
-    println("-= Array =-")
-    val a1 = Array(3) { it }
-    println("Размер массива: ${a1.size}")
-    println("Итерация по массиву:")
-    for (element in a1) { println(element) }
-    println("Чтение элемента по индексу 1: ${a1[1]}")
-    // Можно изменить элемент массива
-    a1[1] = 100
-    println("Массив после изменения элемента по индексу 1: ${a1.joinToString()}")
-    // Добавить элемент в массив нельзя - только создать новый, скопировав старый
-    val a2 = a1.plus(200)
-    println("Новый массив : ${a2.joinToString()}")
+    val abs = createAbsList()
+    println(abs)
+}
 
-    println("-= List =-")
-    val l1 = listOf(1, 2, 3)
-    println("Размер списка: ${l1.size}")
-    println("Элементы списка:")
-    for (element in l1) { println(element) }
-    println("Чтение элемента по индексу 1: ${l1[1]}")
-    // Элемент List изменить нельзя
-    // Добавить элемент в List нельзя - только создать новый, скопировав старый
-    val l2 = l1.plus(100)
-    println("Новый список : $l2")
+fun createAbsList(): List<String> {
+    val result = mutableListOf(
+        "Полдень, XXII век (1962)"
+    )
 
-    println("-= MutableList =-")
-    val ml = mutableListOf(0, 1, 2)
-    println("MutableList - это List? ${ ml is List<Int> }")
-    // Можно изменить элемент MutableList
-    ml[1] = 100
-    println("MutableList после изменения элемента по индексу 1: $ml")
-    // В MutableList можно добавить элемент
-    ml.add(300)
-    println("MutableList после добавления элемента: $ml")
+    // добавить в конец
+    result.add("Обитаемый остров (1969)")
+
+    // вставить в середину
+    result.add(1, "Трудно быть богом (1964)")
+
+    // добавить другой список
+    result.addAll(listOf(
+        "Жук в муравейнике» (1980)",
+        "Волны гасят ветер» (1986)"
+    ))
+
+    return result
 }
