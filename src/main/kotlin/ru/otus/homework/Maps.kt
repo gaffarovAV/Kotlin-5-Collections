9524e8629c378b797d3e90e301dad24d48bf183f
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-17 14:31:36 +0200

    Good hash
    (cherry picked from commit 17e6c46553cd73800e422e1e3d7089170eba05e9)
    (cherry picked from commit 2d8a2320d02004177817a8113b9a5d7ddc9b8b80)
    (cherry picked from commit ce3fc0f4e55727b3d003a5be5900fafa0c1c5469)
    (cherry picked from commit 007bfa8fdae62dbab17f1067f7782f92ed67f1ae)

--- src/main/kotlin/ru/otus/homework/Maps.kt
+++ src/main/kotlin/ru/otus/homework/Maps.kt
@@ -5,41 +5,9 @@ import kotlin.system.measureNanoTime
 fun main() {
     val absList = createAbsList().mapToBook()
     val unwrittenBook = Book("Книга которой нет", 2023)
-    println("Время поиска в List: " + measureNanoTime { absList.contains(unwrittenBook) })
-
-    val absMap = absList.associateBy { it.year }
-    println("Время поиска в Map by году: " + measureNanoTime { absMap[unwrittenBook.year] })
 
     Book.logHashcode = false
     val absYearMap = absList.associateWith { it.year }
     Book.logHashcode = true
     println("Время поиска года в Map по книге: " + measureNanoTime { absYearMap[unwrittenBook] })
-
-    val numMap: Map<String, Int> = mapOf(
-        "Один" to 1,
-        "Два" to 2,
-        "Три" to 3
-    )
-    println("Дай мне цифру `Два`: " + numMap["Два"]) // 2
-    println("Дай мне цифру `Пять`: " + numMap["Пять"]) // null
-    println("Дай мне цифру `два`: " + numMap["два"]) // null
-
-    val listToAdd: List<Pair<String, Int>> = listOf(
-        "Четыре" to 444,
-        "Четыре" to 44,
-        "Четыре" to 4
-    )
-    println("listToAdd: $listToAdd") // [(Четыре, 444), (Четыре, 44), (Четыре, 4)]
-    val numMap2 = numMap + listToAdd
-    println("numMap2: $numMap2") // {Один=1, Два=2, Три=3, Четыре=4}
-    val numMap3 = numMap2 + ("Пять" to 5)
-    println("numMap3: $numMap3") // {Один=1, Два=2, Три=3, Четыре=4, Пять=5}
-    val numMap4 = numMap3 - "Четыре"
-    println("numMap4: $numMap4") // {Один=1, Два=2, Три=3, Пять=5}
-
-    val upperKey = numMap.mapKeys { (key, _) -> key.uppercase() }
-    println("upperKey: $upperKey") // {ОДИН=1, ДВА=2, ТРИ=3}
-
-    val plus100 = numMap.mapValues { (_, value) -> value + 100 }
-    println("plus100: $plus100") // {Один=101, Два=102, Три=103}
 }
--- src/main/kotlin/ru/otus/homework/Sets.kt
+++ src/main/kotlin/ru/otus/homework/Sets.kt
@@ -5,28 +5,9 @@ import kotlin.system.measureNanoTime
 fun main() {
     val absList = createAbsList().mapToBook()
     val unwrittenBook = Book("Книга которой нет", 2023)
-    println("Время поиска в List: " + measureNanoTime { absList.contains(unwrittenBook) })
 
     Book.logHashcode = false
     val absSet = absList.toSet()
     Book.logHashcode = true
     println("Время поиска в Set: " + measureNanoTime { absSet.contains(unwrittenBook) })
-
-    val numSet: Set<Int> = setOf(1, 2, 3)
-    println("2 находится в наборе: " + numSet.contains(2)) // true
-    println("5 находится в наборе: " + numSet.contains(5)) // false
-
-    val listToAdd = listOf(4, 4, 4, 4, 4)
-    println("listToAdd: $listToAdd") // [4, 4, 4, 4, 4]
-    val numSet2 = numSet + listToAdd
-    println("numSet2: $numSet2") // [1, 2, 3, 4]
-    val numSet3 = numSet2 + 5
-    println("numSet3: $numSet3") // [1, 2, 3, 4, 5]
-    val numSet4 = numSet3 - 1
-    println("numSet4: $numSet4") // [2, 3, 4, 5]
-
-    val diff = setOf(1, 2, 3, 4, 5) - setOf(2, 4)
-    println("diff: $diff") // [1, 3, 5]
-    val sum = setOf(1, 3, 5) + setOf(2, 4)
-    println("sum: $sum") // [1, 3, 5, 2, 4]
 }
