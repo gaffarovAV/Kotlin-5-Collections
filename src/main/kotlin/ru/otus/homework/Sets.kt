7cc65525037cb09958e180c21ef602098faee80d
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-17 12:22:33 +0200

    Set has unique values
    (cherry picked from commit 3d15034ccfbb916f8f00df1f7141621f3a080b86)
    (cherry picked from commit 6c00ba292e7b1c65f732966275d02209cd7bcfe5)
    (cherry picked from commit e62efd7faf07ce29d59261afa091975c741f334c)
    (cherry picked from commit 0b659a480cc565c0c1de6bb2beca69d129578620)

--- src/main/kotlin/ru/otus/homework/Sets.kt
+++ src/main/kotlin/ru/otus/homework/Sets.kt
@@ -15,4 +15,9 @@ fun main() {
     val numSet: Set<Int> = setOf(1, 2, 3)
     println("2 находится в наборе: " + numSet.contains(2)) // true
     println("5 находится в наборе: " + numSet.contains(5)) // false
+
+    val listToAdd = listOf(4, 4, 4, 4, 4)
+    println("listToAdd: $listToAdd") // [4, 4, 4, 4, 4]
+    val numSet2 = numSet + listToAdd
+    println("numSet2: $numSet2") // [1, 2, 3, 4]
 }
