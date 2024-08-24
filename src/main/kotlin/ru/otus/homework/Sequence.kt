827dafd48177a0db94d12687d8b31cfd4744798b
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-24 16:58:47 +0200

    Collection transformation

--- src/main/kotlin/ru/otus/homework/Sequence.kt
+++ src/main/kotlin/ru/otus/homework/Sequence.kt
@@ -3,17 +3,26 @@ package ru.otus.homework
 import kotlin.random.Random
 
 fun main() {
-    val list = createList()
+    val list = createList(10)
     println("Source list:")
     println(list)
+
+    println("Collection transformation:")
+    println(list.collectionTransform())
 }
+
+fun List<RandomNumber>.collectionTransform(take: Int = 5) =
+    filter { random -> random.filter { it >= 0 } }
+        .map { it.stringify() }
+        .take(take)
+        .toSet()
 
 fun createList(length: Int = 10): List<RandomNumber> {
     return (1..length).map { RandomNumber() }
 }
 
 class RandomNumber {
-    val value: Int = Random.nextInt(-100, 100)
+    val value: Int = Random.nextInt(-10, 10)
 
     init {
         println("Created: $value")
