1dc43311dbec4e5bcc69d784620c555923bed185
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-08-28 11:09:40 +0200

    Homework

---
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -0,0 +1,6 @@
+package ru.otus.homework.mapswap
+
+/**
+ * Меняет местами ключи и значения
+ */
+fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
---
+++ src/main/kotlin/ru/otus/homework/persons/Person.kt
@@ -0,0 +1,9 @@
+package ru.otus.homework.persons
+
+/**
+ * Запись в справочнике
+ * @property name Имя
+ * @property surname Фамилия
+ * @property age Возраст
+ */
+data class Person(val name: String, val surname: String, val age: Int)
