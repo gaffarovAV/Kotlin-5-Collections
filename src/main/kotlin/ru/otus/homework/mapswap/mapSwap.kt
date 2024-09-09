bcab2af1d54a6569ca871496dc8c5cfefb372a2f
Andrey Gaffarov <a.gaffarov@yadro.com>
2024-09-09 13:42:03 +0300

    Yadro_gaffarov_av_HomeWork_02_
     -  Implimentation of the NaturalList::subList(), containsAll()
     -  Implimentation of the Map::swap()
     -  Implimentation of the Person::sortByAge(), sortByName()
    
    Ps
    -  Implimentation of the NaturalList::equals(),hashCode() without contracts

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,15 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K>
+    {
+        val tempMap: MutableMap<V, K> = mutableMapOf<V, K>()
+
+        for ((key, value) in entries) {
+            if(!containsKey(key))
+                tempMap.put(value,key)
+            else
+                tempMap.set(value,key)  // is it needed?
+        }
+        return tempMap.toMap()
+    }
